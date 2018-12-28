01ccd2e920615b082d3f4cec44ad08f7576059a6
Arunava <learningdroidarunava@gmail.com>
2018-12-28 20:28:57 +0530

    Added code to sample from the model :)

--- utils.py
+++ utils.py
@@ -21,3 +21,66 @@ def clip(gradients, max_val):
                  'dby'  : dby}
 
     return gradients
+
+# Function to sample from the model
+def sample(parameters, char2int):
+    """
+    Sample a sequence of characters according to a sequence of probability
+    distributions output of the RNN
+    """
+
+    # Retrieve parameters and relevant shapes from parameters
+    Waa = parameters['Waa']
+    Wax = parameters['Wax']
+    Wya = parameters['Wya']
+    Wba = parameters['Wba']
+    Wby = parameters['Wby']
+
+    # Get vocab_size
+    vocab_size = by.shape[0]
+    n_a = Waa.shape[1]
+
+    # Create a one-hot vector x for the first character
+    x = np.zeros((vocab_size, 1))
+
+    # Initialize a_prev as zeros
+    a_prev = np.zeros((n_a, 1))
+
+    # Create a list which will keep the sampled indices
+    indices = []
+
+    # idx is a flag to detect a newline character
+    idx = -1
+
+    # Set the counter to initial value
+    # This is the value to check the number of sampled characters
+    counter = 0
+
+    # Getting the idx of the newline char
+    newline_char = char2int['\n']
+
+    # Loop over timesteps and sample at each timestep
+    while (idx != newline_char and counter != 50):
+        # Forward Prop
+        a = np.tanh(np.dot(Wax, x) + np.dot(Waa, a_prev) + b)
+        z = np.dot(Wya, a) + by
+        y = softmax(z)
+
+        # Getting the idx with probability 'y'
+        idx = np.random.choice(np.arange(vocab_size), p=y.ravel())
+
+        # Append to the indices
+        indices.append(idx)
+
+        # Overwrite the input character
+        x = np.zeros((vocab_size, 1))
+        x[idx] = 1
+
+        # Update 'a_prev' to be 'a'
+        a_prev = a
+
+    # If we have sampled for 50 times then adding a newline character
+    if counter == 50:
+        indices.append(char2int['\n'])
+
+    return indices
